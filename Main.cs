45e696efd38b63656855afe02765f8bf345d5c30
Benjamin Nguyen <xccr123@gmail.com>
2026-07-31 14:35:13 -0700

    update vers number

--- Main.cs
+++ Main.cs
@@ -8,7 +8,7 @@ using UnityEngine;
 using ModUtil;
 using Presets;
 
-[assembly: MelonInfo(typeof(Mod), "M2 Bradley Extended", "0.9.5B", "ATLAS")]
+[assembly: MelonInfo(typeof(Mod), "M2 Bradley Extended", "0.9.6", "ATLAS")]
 [assembly: MelonGame("Radian Simulations LLC", "GHPC")]
 
 namespace M2BradleyExtended
